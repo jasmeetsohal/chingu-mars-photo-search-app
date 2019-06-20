0131a0494cc0eab42a6319152a4b59b60ea7317c
jasmeetsohal <jasmeetsohal93@gmail.com>
2019-06-20 21:32:30 +0530

    implement front-end framework of Reactjs

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./server/routes/index');
 var marsRouter =  require('./server/routes/mars');
 
 var app = express();
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', indexRouter);
 app.use('/mars',marsRouter);
